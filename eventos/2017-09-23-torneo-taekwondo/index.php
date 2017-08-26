cb21b17df56b761ef1293ec3620ae4a932f3f7e5
Axel Barragan <axelbarragan@gmail.com>
2017-08-26 17:40:17 -0500

    Avances
    Avances

--- eventos/2017-09-23-torneo-taekwondo/index.php
+++ eventos/2017-09-23-torneo-taekwondo/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('../config/config.php');
+include_once('../../config/config.php');
 
 ?>
 <!DOCTYPE html>
@@ -14,7 +14,7 @@ include_once('../config/config.php');
   <meta name="theme-color" content="#04e4a0">
   <meta name="keywords" content="pendiente">
   <title>Próximos eventos | Esencia Teotihuacan</title>
-  <?php include_once('../linkcss.php'); ?>
+  <?php include_once('../../linkcss.php'); ?>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -23,8 +23,8 @@ include_once('../config/config.php');
     <![endif]-->
   </head>
   <body>
-    <div class="preloader"><img src="../assets/img/loading.gif"/><span class="sr-only">Loading...</span></div>
-    <?php include_once('../menu.php'); ?>
+    <div class="preloader"><img src="../../assets/img/loading.gif"/><span class="sr-only">Loading...</span></div>
+    <?php include_once('../../menu.php'); ?>
     <section id="intro" class="intro">
       <div class="header-content">
       </div>
@@ -33,7 +33,7 @@ include_once('../config/config.php');
       <div class="container">
         <div class="row">
           <div class="col-lg-12 pad-bot-10">
-          <h2><a href="../index.php">INICIO</a> > PRÓXIMOS EVENTOS</h2>
+          <h2><a href="<?php echo LINK; ?>">INICIO</a> > <a href="<?php echo LINK; ?>eventos">PRÓXIMOS EVENTOS</a> > </h2>
             <hr class="concolor">
           </div>
           <div class="col-lg-12 fondo-gris">
@@ -47,32 +47,10 @@ include_once('../config/config.php');
               <a href="2017-09-23-torneo-taekwondo" class="btn btn-primary">Ver más</a>
             </div>
           </div>
-          <div class="col-lg-12 fondo-gris">
-            <div class="col-lg-6 pad-evento">
-              <img class="img-responsive center-block imagen-evento" src="../assets/img/eventos/LONA 2x2 JIRAFA.jpg">
-            </div>
-            <div class="col-lg-6 pad-evento texto-blanco">
-              <h3>1ER CARRERA CAMINATA REINO ANIMAL RUN “AL RESCATE DE LA BIODIVERSIDAD”</h3>
-              <h4>Domingo 08 de Octubre 2017</h4>
-              <p>Es una carrera que comprende una ruta de 7 km y una caminata familiar de 3k diseñada para vivir una experiencia extrema junto a más de 1,400 animales en semi-libertad dentro de un ambiente natural. Al rescate de nuestra Biodiversidad.</p>
-              <a href="2017-10-08-carrera-reino-animal" class="btn btn-primary">Ver más</a>
-            </div>
-          </div>
-          <div class="col-lg-12 fondo-gris">
-            <div class="col-lg-6 pad-evento">
-              <img class="img-responsive center-block imagen-evento" src="../assets/img/eventos/volante FTE@2x.png">
-            </div>
-            <div class="col-lg-6 pad-evento texto-blanco">
-              <h3>4TA CARRERA DEL PULQUE 2017 EDOMEX</h3>
-              <h4>Domingo 29 de Octubre 2017</h4>
-              <p>4ta Feria y Carrera del Pulque ”Una Carrera con Tradición”, promoviendo el rescate de las tradiciones del cultivo del maguey, la producción del pulque y la conservación del Acueducto del Padre Tembleque que es Patrimonio Cultural de la Humanidad, en el Municipio de Nopaltepec.</p>
-              <a href="2017-10-29-4ta-carrera-pulque" class="btn btn-primary">Ver más</a>
-            </div>
-          </div>
         </div>
       </div>
     </section>
-    <?php include_once('../footer.php'); ?>
-    <?php include_once('../scripts.php'); ?>
+    <?php include_once('../../footer.php'); ?>
+    <?php include_once('../../scripts.php'); ?>
   </body>
   </html>
